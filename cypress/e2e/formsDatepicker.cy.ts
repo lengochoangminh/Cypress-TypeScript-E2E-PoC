06fdaf50932688ea493097dd43c25e776d81a9dc
Minh.Le <lengochoangminh@gmail.com>
2024-06-25 15:03:25 -0700

    Fixed the failure

--- cypress/e2e/formsDatepicker.cy.ts
+++ cypress/e2e/formsDatepicker.cy.ts
@@ -2,10 +2,7 @@ import { datePickerPage } from "../pages/datepickerPage";
 
 describe("Forms - Datepicker", () => {
   it("Verify to select the date from the Common Datepicker", () => {
-    const date = new Date();
-    date.setDate(date.getDate() + 7);
-
     datePickerPage.navigateTo();
-    datePickerPage.inputCommonDatePicker(date);
+    datePickerPage.inputCommonDatePicker();
   });
 });
--- cypress/pages/datepickerPage.ts
+++ cypress/pages/datepickerPage.ts
@@ -2,33 +2,23 @@ export class DatePickerPage {
   get txtCommonDatePicker() {
     return cy.get("input[placeholder='Form Picker']");
   }
-  get calendarDayCell() {
-    return cy.get(".day-cell.ng-star-inserted").find(".cell-content");
+  get calendarTodayCell() {
+    return cy.get(".today.day-cell.ng-star-inserted");
   }
 
   navigateTo() {
     cy.visit(Cypress.env("baseUrl") + "/pages/forms/datepicker");
   }
 
-  inputCommonDatePicker(date: Date) {
+  inputCommonDatePicker() {
+    const date = new Date();
     const expectedDate = date.getDate().toString();
     const expectedMonthShot = date.toLocaleString("En-US", { month: "short" });
-    // const expectedMonthLong = date.toLocaleString("En-US", { month: "long" });
     const expectedYear = date.getFullYear();
     const dateToAssert = `${expectedMonthShot} ${expectedDate}, ${expectedYear}`;
-    // const expectedMonthAndYear = ` ${expectedMonthLong} ${expectedYear}`;
 
     this.txtCommonDatePicker.click();
-    let count = 0;
-    this.calendarDayCell.each(($e1, index) => {
-      const text = $e1.text();
-      if (text.match(expectedDate)) {
-        if (count == 1) this.calendarDayCell.eq(index).click();
-        else {
-          count++;
-        }
-      }
-    });
+    this.calendarTodayCell.click();
 
     expect(this.txtCommonDatePicker.should("have.value", dateToAssert));
   }
